96472dfb2b92f20d1562c20eb469cf7c922ea44e
medilore <fc2tokaqq+medilore@gmail.com>
2022-12-19 15:12:10 +0900

    change button context

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -111,8 +111,8 @@
                 <x-add-song-modal :creators="$creators" />
                 <div class="w-full flex justify-center py-12" id="button">
                     <button id="modalopener"
-                        class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 mx-auto transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-4 sm:px-8 py-2 text-xs sm:text-sm">Open
-                        Modal</button>
+                        class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 mx-auto transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-4 sm:px-8 py-2 text-xs sm:text-sm">Add
+                        Song</button>
                 </div>
             @else
                 <p>
